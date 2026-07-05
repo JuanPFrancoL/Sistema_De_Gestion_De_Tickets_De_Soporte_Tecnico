fc48b5da68c677a8891558cb94d35ff1b21c4731
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-05 15:05:42 -0500

    Se realiza la clase cola

--- src/main/java/org/example/Queue.java
+++ src/main/java/org/example/Queue.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class Queue {
+    Node head;
+    Node tail;
+    int size;
+
+    public Queue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void enqueue(Node node) {
+        if (size == 0) {
+            tail = node;
+            head = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        size++;
+    }
+
+    public Node dequeue() {
+        if (size == 0) {
+            throw new RuntimeException("Queue is empty");
+        }
+        Node nodeDequeue = head;
+        head = head.next;
+        size--;
+        return nodeDequeue;
+    }
 }
