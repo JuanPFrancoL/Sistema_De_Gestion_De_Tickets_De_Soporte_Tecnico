5f37a0b77d3b7e871702bf34f8e82de71f41e9c7
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-05 16:07:58 -0500

    Se añaden imagenes al panel de inicio de sesion

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login {
     private JLabel lblLogin;
@@ -10,6 +11,33 @@ public class Login {
     private JPasswordField txtPassword;
     private JButton btnLogin;
     private JPanel panelLogin;
+    private JLabel lblUserImage;
+    private JLabel lblPasswordImage;
+
+    public Login() {
+
+        ImageIcon icon = new ImageIcon(getClass().getResource("/images/user.png"));
+
+        Image scaledImg = getScaledImg(icon);
+
+        lblUserImage.setIcon(new ImageIcon(scaledImg));
+
+        ImageIcon icon2 = new ImageIcon(getClass().getResource("/images/password.png"));
+
+        Image scaledImg2 = getScaledImg(icon2);
+
+        lblPasswordImage.setIcon(new ImageIcon(scaledImg2));
+
+    }
+
+    public Image getScaledImg(ImageIcon icon) {
+        if (icon == null) {
+            return null;
+        }
+        return icon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+
+    }
+
 
     public JPanel getPanelLogin() {
         return panelLogin;
