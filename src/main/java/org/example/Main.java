b0c59e7dca6d13cd10c755301201c85165b65fe5
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-07 14:33:10 -0500

    Se realiza cambio en la interfaz de admin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public Main() {
         FileManager fileManager = new FileManager();
