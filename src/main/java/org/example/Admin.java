3db908e8996599d1ec4abac279e932ee6a09427a
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-07 00:53:05 -0500

    Se realiza la interfaz grafica del panel de administradores

---
+++ src/main/java/org/example/Admin.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Admin {
+    private JPanel panelPrincipalAdmin;
+    private JLabel lblTitleAdmin;
+    private JButton btnChangePriority;
+    private JButton btnSearchTicket;
+    private JButton btnCloseTicket;
+    private JButton btnSeachPendingTickets;
+    private JButton btnShowTicketsInAtention;
+    private JButton btnChangeStatus;
+    private JButton btnUndoLastStateChange;
+    private JButton btnShowTicketStatusHistory;
+    private JButton btnClose;
+    private JLabel lblPanelStatus;
+
+
+    public JPanel getPanelPrincipalAdmin() {
+        return panelPrincipalAdmin;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,8 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
-        frame.add(new Login().getPanelLogin());
+//        frame.add(new Login().getPanelLogin());
+        frame.add(new Admin().getPanelPrincipalAdmin());
         frame.setVisible(true);
     }
 }
