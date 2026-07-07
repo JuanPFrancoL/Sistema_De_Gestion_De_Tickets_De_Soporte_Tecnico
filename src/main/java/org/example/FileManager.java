8b00062287241f5716423da43358af4aaf34961b
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-06 21:53:42 -0500

    Se crea metodo para leer el archivo

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileManager {
     File file;
     SimpleList simpleList = new SimpleList();
+
     public void readFile(File file) {
         try {
-            
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                simpleList.addNode(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+    
 }
